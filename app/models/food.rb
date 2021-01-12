5f75a42bd2b6ddce7672228d798f060208135db9
Thiago Tenório <thiago.tenorio@rogalabs.com>
2021-01-11 23:05:02 -0300

    feat: annotate food and category

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: foods
+#
+#  id          :bigint           not null, primary key
+#  description :string           not null
+#  title       :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint           not null
+#
+# Indexes
+#
+#  index_foods_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#
 class Food < ApplicationRecord
   belongs_to :category
 
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: foods
+#
+#  id          :bigint           not null, primary key
+#  description :string           not null
+#  title       :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint           not null
+#
+# Indexes
+#
+#  index_foods_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
