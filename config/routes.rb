741634a287b5597f02a1d0702c5d794a6276ec2b
Thiago Tenório <thiago.tenorio@rogalabs.com>
2021-01-12 14:16:41 -0300

    feat: added users and rating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :categories
   resources :foods
+  resources :ratings
+  resources :users do
+    post 'evaluate_food/:food_id', to: 'users#evaluate_food', on: :member
+  end
 end
